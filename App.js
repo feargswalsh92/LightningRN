638ea2d0072c5a90dabef233f468d6b92ce93732
Feargal Walsh <feargswalsh@gmail.com>
2018-05-10 16:18:39 -0700

    Fetch request working and populating data in app

--- App.js
+++ App.js
@@ -10,61 +10,56 @@ import {
 } from 'react-native';
 import { ListItem } from 'react-native-elements'
 
+export default class App extends React.Component {
 
-export default class App extends Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-  isLoading: false,
-  data : [{
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  }]
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+  keyExtractor= (item, index) => index
+
+  renderItem = ({ item }) => (
+    <ListItem 
+     title = {item.title}
+     subtitle = {item.year}
+     hideChevron = {true}
+     />
+  )
+
+  render(){
+   return (
+    this.state.isLoading ? 
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+     : <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={this.renderItem}
+          keyExtractor = {this.keyExtractor}
+        />
+      </View>
+    );
   }
 }
-  
-  keyExtractor = (item, index) => index
-  
-  renderItem = ({ item }) => (
-    <ListItem
-      title={item.name}
-      subtitle={item.subtitle}
-      roundAvatar
-      avatar={{ source: { uri: item.avatar_url } }}
-    />
-  )
-  render() {
-    const spinner = <ActivityIndicator size='large'/>
-     return (
-    this.state.isLoading ? 
-    <View styles = {styles.container}>
-      {spinner}
-      </View> 
-      : <View styles = {styles.container}>
-          <FlatList
-      keyExtractor={this.keyExtractor}
-      data={this.state.data}
-      renderItem={this.renderItem}
-      />
-    </View>
-  )
-}
-}
-
 
 AppRegistry.registerComponent('AwesomeProject', () => App);
 
-
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%'
-  }
-})
